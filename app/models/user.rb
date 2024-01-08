30374e951f2e6153f04325ae921be3f9b4f5acce
Garrett <gcamoin5@gmail.com>
2024-01-08 16:17:47 -0500

    added secure password macro

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :restaurants, through: :reviews
 
+    has_secure_password
+
     validates :user_name, uniqueness: true
     validates :password_digest, length:{minimum: 5}
 end
